4680890ca45f65a168b50358c735f94a54f9663a
Bonavee <67125950+Bonavee@users.noreply.github.com>
2020-08-05 00:15:42 +0100

    Add files via upload

---
+++ SpeakHello.js
@@ -0,0 +1,9 @@
+
+( function (window) {
+  var speakWord = "Hello";
+  var helloSpeaker = {};
+  helloSpeaker.speak = function(names) {
+    console.log(speakWord + " " + names);
+  }
+  window.helloSpeaker = helloSpeaker;
+})(window);
